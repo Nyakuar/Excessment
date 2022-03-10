1f629e76add0b6058a80dc2649c45d5701bb4abe
Nyakuar <nyakuaryy@gmail.com>
2022-03-10 08:59:00 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,46 @@
+fun main(){
+
+    school ()
+    nameandAge()
+    length()
+    sub(23 23)
+    println(x)
+
+}
+
+fun school(){
+    var name="Codehive"
+    println(name[5].toString()+name[6]+name[7])
+
+}
+
+
+fun sub(num1: num2: Int)(){
+    x = 23 - 23
+    println(x)
+
+}
+
+
+fun nameandAge(){
+    var name= "Hi my name is Nyakuar i am"
+    var age= "22 years old"
+    var statement= name+" "+ age
+    println(statement )
+
+
+}
+
+
+fun length(){
+    var Job = "engineer"
+    println(Job.length)
+}
+
+
+
+
+
+
+
+
